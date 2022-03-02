6888d3749624cdb1c96da910551471d7f1a78d94
mirrormouse <kagamidokei@gmail.com>
2022-03-02 13:02:55 +0900

    test commit

---
+++ bmi.py
@@ -0,0 +1,12 @@
+from browser import document
+
+def calc_bmi(e):
+    weight = float(document["weight"].value)
+    height = float(document["height"].value)
+
+    bmi = str(weight/(height*height))
+    rslt = document["result"]
+    rslt.text = bmi
+
+execute_btn = document["execute"]
+execute_btn.bind("click", calc_bmi)
